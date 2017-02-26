518490f5b841f77d38d23e1a6046498c2ff53611
rtwik <rtwik@UNKNOWN.Home>
2017-02-27 00:01:48 +0530

    pairs collected

--- NNText/NLPinfo.py
+++ NNText/NLPinfo.py
@@ -16,7 +16,7 @@ verbtags = ['VB','VBD','VBG','VBN','VBP','VBZ', 'CD',]
         
 #markers=['a','an','numerl','every','each','many','few','much','little','a lot of','numeral +pl','sing+clas']
 
-markers=['a','an','every','each','many','few','much','little']
+markers=['a','an','every','each','many','few','much','lot']
 
 import os 
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -26,16 +26,25 @@ f = open(dir_path + '/data/POStags.csv','r')
 tags=[i.strip('\n') for i in f.readlines()]
 f.close()
 
+pairs = []
 clean_sents =[] 
 for i in tag_sent:
-    sents = []
     nouns = 0
     record = False
+    pos=0
+    nn = None
+    mr=None
     for j in i:
-        if j[1] in ['NN','NNS']:
+        if 'NN' in j[1]:
             nouns = nouns + 1     
-        if j[0] in markers:            
+            nn = j[0]
+            pos_n = pos
+        if j[0] in markers and nouns <1:            
            record = True
-    if record == True and nouns < 2:
+           mr = j[0]
+           pos_m = pos
+        pos = pos +1
+    if record == True and nouns < 2 and nn != None:
        clean_sents.append(i)
+       pairs.append([mr,nn])
         
