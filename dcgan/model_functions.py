7c87e90fefd8c1f26b5636855e0d999fba869fe1
rtwik <rittyun@yahoo.com>
2021-12-27 19:03:50 +0530

    dropout added

--- dcgan/model_functions.py
+++ dcgan/model_functions.py
@@ -128,18 +128,22 @@ class Generator(nn.Module):
             nn.ConvTranspose2d( config['nz'], config['ngf'] * 8, 4, 1, 0, bias=False),
             nn.BatchNorm2d(config['ngf'] * 8),
             nn.ReLU(True),
+            nn.Dropout(0.5),
             # state size. (ngf*8) x 4 x 4
             nn.ConvTranspose2d(config['ngf'] * 8, config['ngf'] * 4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(config['ngf'] * 4),
             nn.ReLU(True),
+            nn.Dropout(0.5),
             # state size. (ngf*4) x 8 x 8
             nn.ConvTranspose2d( config['ngf'] * 4, config['ngf'] * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(config['ngf'] * 2),
             nn.ReLU(True),
+            nn.Dropout(0.5),
             # state size. (ngf*2) x 16 x 16
             nn.ConvTranspose2d( config['ngf'] * 2, config['ngf'], 4, 2, 1, bias=False),
             nn.BatchNorm2d(config['ngf']),
             nn.ReLU(True),
+            nn.Dropout(0.5),
             # state size. (ngf) x 32 x 32
             nn.ConvTranspose2d( config['ngf'], config['nc'], 4, 2, 1, bias=False),
             nn.Tanh()
@@ -159,18 +163,22 @@ class Discriminator(nn.Module):
             # input is (nc) x 64 x 64
             nn.Conv2d(config['nc'], config['ndf'], 4, 2, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(0.5),
             # state size. (ndf) x 32 x 32
             nn.Conv2d(config['ndf'], config['ndf'] * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(config['ndf'] * 2),
             nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(0.5),
             # state size. (ndf*2) x 16 x 16
             nn.Conv2d(config['ndf'] * 2, config['ndf'] * 4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(config['ndf'] * 4),
             nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(0.5),
             # state size. (ndf*4) x 8 x 8
             nn.Conv2d(config['ndf'] * 4, config['ndf'] * 8, 4, 2, 1, bias=False),
             nn.BatchNorm2d(config['ndf'] * 8),
             nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout(0.5),
             # state size. (ndf*8) x 4 x 4
             nn.Conv2d(config['ndf'] * 8, 1, 4, 1, 0, bias=False),
             nn.Sigmoid()
